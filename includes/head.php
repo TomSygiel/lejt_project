e7bbc90791d0e5edbd330b8be5e18560d417d12e
lindastorgard <linda.storgard@gmail.com>
2018-11-23 11:01:30 +0100

    add head.php footer.php header.php
    add mediaqueries in scss connect css stylesheet add includes in index.php

---
+++ includes/head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i|Open+Sans:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    
+    <!-- Own CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    
+    <title>Millhouse</title>
+</head>
+    
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+
+<!-- include Head -->
+<?php include 'includes/head.php';?>
+
+
+<body>
+    
+<h1>hej</h1>
+
+
+<!-- include Head -->
+<?php include 'includes/footer.php';?>
