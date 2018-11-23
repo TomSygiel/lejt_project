b24b857dcd5393756e565c2e94fae20f67f0fe04
eunjuhuss <eunjuhuss@hotmail.com>
2018-11-23 14:55:59 +0100

    Add stuff

--- views/register.php
+++ views/register.php
@@ -6,30 +6,25 @@ session_start();
 <?php include '../includes/head.php';?>
 
 
-
 <body> 
     <div class="row">
         <div class="col-12 col-md-12 border-primary text-center card" style="max-width: 30rem;">
 
-
-            <form action="./includes/login.php" method="POST">
-               <h2>Login</h2>
+            <form action="../includes/register_server.php" method="POST">
+                <h2>Register</h2>
                 <label for="login_username">username</label>
                 <input type="text" name="username" placeholder="username" id="login_username">
                 <label for="login_password">password</label>
                 <input type="password" name="password" placeholder="password" id="login_password">
                 <input type="hidden" name="user_ID" id="user_ID">
-                <input type="submit" value="Login" class="btn btn-primary" >
+                <input type="submit" value="submit" class="btn btn-primary" >
             </form>
+
         </div>
     </div>
 
+        </body> 
+    <!-- include Head -->
+    <?php include '../includes/footer.php';?> 
 
-    <h3>not a member? register here</h3>
-    <form action="register.php" method="POST"> 
-        <input type="submit" value="submit" class="btn btn-primary"></form>
-
-</body>  
-<?php include '../includes/footer.php';?> 
-
-</Html>
+    </Html>
