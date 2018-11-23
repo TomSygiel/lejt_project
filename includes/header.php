91295b639a1ab74da6541600c19581a7760c3c41
lindastorgard <linda.storgard@gmail.com>
2018-11-23 14:31:45 +0100

    add php url for logo

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,11 @@
+<?php 
+    include 'siteurl.php';
+?>
+
 <header class="row">
     <div class="col-12">
         <a href="index.php"></a>
-        <img src="../images/logo_light.png" alt="Millhouse logo">
+        <img src="<?= $SITE_URL ?>/images/logo_light.png" alt="Millhouse logo">
         <h2>haloj</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint reprehenderit libero cum! Reiciendis iste labore eaque magni assumenda, itaque atque veniam odio et eveniet minima debitis, iure maiores totam maxime.</p>
     </div>
