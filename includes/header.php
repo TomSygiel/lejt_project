13eb71e066179ca80f150c23b3b7b129e1c0c719
lindastorgard <linda.storgard@gmail.com>
2018-11-23 14:18:59 +0100

    local changest

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<header class="row">
+    <div class="col-12">
+        <a href="index.php"></a>
+        <img src="../images/logo_light.png" alt="Millhouse logo">
+        <h2>haloj</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint reprehenderit libero cum! Reiciendis iste labore eaque magni assumenda, itaque atque veniam odio et eveniet minima debitis, iure maiores totam maxime.</p>
+    </div>
+
+    <nav>
+    </nav>
+</header>
