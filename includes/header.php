a02b1521270ced1da668323a1a2a03d353987e6a
lindastorgard <linda.storgard@gmail.com>
2018-11-26 20:57:24 +0100

    fixed broken header

--- includes/header.php
+++ includes/header.php
@@ -1,26 +1,31 @@
 <?php include 'siteurl.php';?>
-<header>
-    <nav class="navbar sticky-top navbar-expand-lg navbar-light navigation_bar">
-        <a class="navbar-brand" href="#"><i class="fas fa-user header_icons"></i></a>
-        <a class="navbar-brand" href="#"><i class="fas fa-pen header_icons"></i></a>
-        <a class="navbar-brand" href="index.php"><img class="navbar_logo" src="<?= $SITE_URL ?>/images/logo_dark.png" alt="Millhouse logo"></a>
-               
-        <button class="navbar-toggler header_button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars header_icons"></i>
-        </button>
 
-        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item nav_item_top">
-                    <a class="nav-link text-uppercase header_links" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-uppercase header_links" href="#">Links</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-uppercase header_links" href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</header>
+<header>
+            <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> vill ändra färgen på navbaren ...gör en egen klass och ta bort bottstrap stylingen-->
+            <nav class="navbar navbar-expand-lg">
+
+                <!--ändra färgerna på rubrikerna i navbaren, jobba med egna klasser eller använd deras vilket är bäst-->
+                <div>
+                <a class="navbar-brand" href="#"><i class="fas fa-user header_icons"></i></a>
+                <a class="navbar-brand" href="#"><i class="fas fa-pen header_icons"></i></a>
+                </div>
+                <a class="navbar-brand navbar_logo" href="index.php"><img class="navbar_image" src="<?= $SITE_URL ?>/images/logo_dark.png" alt="Millhouse logo"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars header_icons"></i>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item nav_item_top">
+                            <a class="nav-link text-uppercase" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-uppercase" href="#">Links</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-uppercase" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
