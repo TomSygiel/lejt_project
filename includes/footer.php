396652a444c04c442d8a7b8d25c95c4c82020051
lindastorgard <linda.storgard@gmail.com>
2018-11-26 11:05:13 +0100

    add footer.scss and index.scss and also changes to footer.php header.php index.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,64 @@
+
+<footer class="footer">
+
+
+    <div class="container text-center text-md-left millhouse_footer">
+      <div class="row">
+        <div class="col-md-4 col-lg-3">
+          <h5 class="my-2">Millhouse</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, vero consectetur? Ea, illum corporis sed dolor!</p>
+        </div>
+   
+        <hr class="w-100 d-md-none">
+
+        <div class="col-md-2 col-lg-2">
+          <h5 class="my-2">About</h5>
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!">Blog</a>
+            </li>
+            <li>
+                <a href="#!">XX</a>
+            </li>
+          </ul>
+        </div>
+
+        <hr class="w-100 d-md-none">
+
+        <div class="col-md-4 col-lg-3">
+          <h5 class="my-2">Address</h5>
+          <ul class="list-unstyled">
+            <li>
+              <p>Tulegatan 41, 113 53 Stockholm</p>
+            </li>
+            <li>
+              <p>blog@millhouse.se</p>
+            </li>
+            <li>
+              <p>07 345 67 544</p>
+            </li>
+          </ul>
+        </div>
+    
+        <hr class="w-100 d-md-none">
+
+        <div class="col-md-2 col-lg-2 text-center">
+          <h5 class="my-2">Follow Us</h5>
+          <a type="button">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a type="button">
+          <i class="fab fa-twitter"></i>
+          </a>
+        <a type="button">
+            <i class="fab fa-instagram"></i>
+        </a>
+        </div>
+      </div>
+    </div>
+</footer>
+
+
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,29 @@
 <?php include 'siteurl.php';?>
+    <header>
+                <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> vill ändra färgen på navbaren ...gör en egen klass och ta bort bottstrap stylingen-->
+               <nav class="navbar sticky-top navbar-expand-lg navbar-light navigation_bar">
+               
+                <!--ändra färgerna på rubrikerna i navbaren, jobba med egna klasser eller använd deras vilket är bäst-->
+                <a class="navbar-brand" href="#"><i class="fas fa-user header_icons"></i></a> 
+                <a class="navbar-brand d-none d-lg-block" href="#"><i class="fas fa-pen header_icons"></i></a>
+                <img src="<?= $SITE_URL ?>/images/logo_dark.png" alt="Millhouse logo"></a>
 
-<header class="row header_flex">
-    <div class="col-12">
-        <a href="index.php"><img src="<?= $SITE_URL ?>/images/logo_light.png" alt="Millhouse logo"></a>
-    </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars header_icons"></i>
+                </button>
 
-    <nav>
-    </nav>
-</header>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item nav_item_top">
+                            <a class="nav-link text-uppercase" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-uppercase" href="#">Links</a>
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link text-uppercase" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
