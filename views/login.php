fc61787c2b23e21f88be94f2230ec0c9c965be99
eunjuhuss <eunjuhuss@hotmail.com>
2018-11-26 12:47:57 +0100

    change code that i did last week

--- views/login.php
+++ views/login.php
@@ -12,13 +12,13 @@ session_start();
         <div class="col-12 col-md-12 text-center card" style="max-width: 30rem;">
 
 
-            <form action="./includes/login.php" method="POST">
+            <form action="../includes/login_server.php" method="POST">
                 <h2>Login</h2>
                 <label for="login_username">username</label><br/>
                 <input type="text" name="username" placeholder="username" id="login_username"><br/>
                 <label for="login_password">password</label><br/>
                 <input type="password" name="password" placeholder="password" id="login_password"><br/>
-                <input type="hidden" name="user_ID" id="user_ID"><br/>
+                <input type="hidden" name="user_id" id="user_id"><br/>
                 <input type="submit" value="Login" class="btn btn-primary" >
             </form>
         </div>
@@ -32,4 +32,4 @@ session_start();
 </body>  
 <?php include '../includes/footer.php';?> 
 
-</Html>
+
--- views/register.php
+++ views/register.php
@@ -1,39 +1,19 @@
-<?php
-session_start();
-include "../includes/db.php";
-
-if(isset($_POST['submit']))
-{
-    
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    if($login->register($username, $password))
-    {
-        header("location:../index.php");
-    }
-    
-    
-    
-}
-?>
-
 <!-- include Head -->
 <?php include '../includes/head.php';?>
+
 
 
 <body> 
     <div class="row text-center justify-content-center">
         <div class="col-12 col-md-12 text-center card" style="max-width: 30rem;">
 
-            <form action="../classes/Register.php" method="POST">
+            <form action="../includes/register_server.php" method="POST">
                 <h2>Register</h2>
                 <label for="login_username">username</label><br/>
                 <input type="text" name="username" placeholder="username" id="login_username"><br/>
                 <label for="login_password">password</label><br/>
                 <input type="password" name="password" placeholder="password" id="login_password"><br/>
-                <input type="hidden" name="user_ID" id="user_ID"><br/>
+                <input type="hidden" name="user_id" id="user_id"><br/>
                 <input type="submit" value="submit" class="btn btn-primary" ><br/>
             </form>
 
