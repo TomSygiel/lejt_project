50a9571dd07a7eb338641e43ff4759f62efa6c6c
eunjuhuss <eunjuhuss@hotmail.com>
2018-11-28 14:24:59 +0100

    progress to change oop code

--- classes/Admin_posts.php
+++ classes/Admin_posts.php
@@ -1,21 +1,10 @@
 <?php
 include ("../includes/database_connection.php");
 
-//data base connection
-/*$pdo = new PDO(
-    "mysql:host=localhost:8889;dbname=millhouse;charset=utf8",
-    "root", //user
-    "root"  //passwordm
-);*/
-
-
-//Prepare a SQL-statement
-
-
 $statement = $pdo->prepare("SELECT * FROM posts");
 //Execute it
 $statement->execute();
-//Fetch every row that it returns. $products is now an Associative array
+//Fetch every row that it returns. $posts is now an Associative array
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 
--- includes/delete_posts.php
+++ includes/delete_posts.php
@@ -5,8 +5,19 @@ session_start();
 include ("database_connection.php");
 
 
+if(isset($_GET['post_id'])){
+    $statement = $pdo->prepare("DELETE FROM posts WHERE post_id = :post_id");
+    // remove product from database
+    $statement->execute(
+        [
+            ":post_id" => $_GET["post_id"]
+        ]
+    );
+   
+}
 
-$statement = $pdo->prepare("DELETE FROM posts WHERE post_id = :post_id");
+
+/*$statement = $pdo->prepare("DELETE FROM posts WHERE post_id = :post_id");
 // remove product from database
 $statement->execute(
     [
@@ -15,5 +26,6 @@ $statement->execute(
 );
 $delete_posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 // efter remove product, go to shoppingcart.php
+*/
 header('Location: ../views/admin_allposts.php');
 ?>
