2272ddb0c360ad650523e643cbdb27f72825f931
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2018-11-29 14:49:04 +0100

    Test connectivity between the blog form, class and database

--- includes/post_server.php
+++ includes/post_server.php
@@ -10,12 +10,20 @@ $title = $_POST["title"];
 $description = $_POST["description"];
 $created_by = $_SESSION["username"];
 
-$temporary_location = $image["tmp_name"];
+/*$temporary_location = $image["tmp_name"];
 
 $new_location = "uploads/" . $image["name"];
 
 $upload_ok = move_upload_file($temporary_location, $new_location);
 
+if($upload_ok){
+    $statement = $pdo->prepare("INSERT INTO images (image, image_text) VALUES (:image, :image_text)");
+    $statement->execute([
+      ":image" => $new_location,
+      ":Image_text"  => $image_text
+    ]);
+}*/
+
 $posts = new Post($pdo);
-$posts->blogPost($image, $title, $description, $created_by, $upload_ok);
+$posts->blogPost($image, $title, $description, $created_by/*, $upload_ok*/);
 $posts->deletePost();
