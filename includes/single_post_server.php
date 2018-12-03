7d985cdfbf7fa3a568c88bf307ccb988683d7919
JuliaLahdo <julia.lahdo@live.se>
2018-12-03 15:19:55 +0100

    modify the OOP of single post

--- includes/single_post_server.php
+++ includes/single_post_server.php
@@ -1,10 +1,18 @@
 <?php
+    require '../includes/database_connection.php';
     include '../classes/Single_post.php';
 
-    require '../includes/database_connection.php';
+    $single_post = new SinglePost($pdo);
+    $single_post->singlepost($title, $description, $created_by, $image, $category, $post_date);
+
+    header('Location: ../views/single_post.php');  
+
+
+
+
 
     /* Check when creating a new post that $_POST["post_id] is sent for the query to work! */
-    $post_id = $_POST["post_id"];
+    /*$post_id = $_POST["post_id"];
 
     //Fetching chosen post to display
     $statement = $pdo->prepare("SELECT title, description, created_by, image FROM posts 
@@ -16,4 +24,4 @@
         ]
     );
 
-    $single_post = $statement->fetchAll(PDO::FETCH_ASSOC);  
+    $single_post = $statement->fetchAll(PDO::FETCH_ASSOC);  */
