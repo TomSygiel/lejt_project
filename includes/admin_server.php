01f3f294bc6a0eef823b07a0fd69207f8c186a79
lindastorgard <linda.storgard@gmail.com>
2018-12-03 15:23:40 +0100

    clean up code and add comments to admin_server.php and admin_allposts.php

--- includes/admin_server.php
+++ includes/admin_server.php
@@ -2,14 +2,10 @@
 
 include ("database_connection.php");
 
-
+//Selects title, created by and date from database
 $statement = $pdo->prepare("SELECT title, created_by, post_date FROM `posts`");
 //Execute it
 $statement->execute();
 //Fetch every row that it returns. $posts is now an Associative array
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
-
-
-<!-- "SELECT * FROM `posts` JOIN users ON posts.post_id = users.user_id" -->
