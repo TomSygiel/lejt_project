fcdc7c190adbddea0d2a235f99d0648ab9257fe2
JuliaLahdo <julia.lahdo@live.se>
2018-12-03 11:35:19 +0100

    add single_post.php
    and also change footer to position absolute as well as change links in header

---
+++ views/single_post.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+?>
+
+<!-- include Head -->
+<?php include '../includes/head.php';?>
+
+<!-- include Header -->
+<?php include '../includes/header.php';?>
+
+<!-- include Footer -->
+<?php include '../includes/footer.php';?>
