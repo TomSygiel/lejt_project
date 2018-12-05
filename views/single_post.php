f409a581044d2873ce2a285b9deb4cb9a798d461
JuliaLahdo <julia.lahdo@live.se>
2018-12-05 14:57:47 +0100

    add more styling to single_post.php-page

--- views/single_post.php
+++ views/single_post.php
@@ -15,9 +15,6 @@ include '../classes/Single_post.php';
 ?>
 <main class="container-fluid single_post_main">  
 
-<!--<div class="container-fluid single_post">
-    <div class="col-10">-->
-
         <?php
 
            $post = new SinglePost($pdo);
@@ -28,7 +25,7 @@ include '../classes/Single_post.php';
             /* Looping through and showing chosen values to display a specific blog post from reaching out to database above. sp_part stands for single post part */
             foreach($array as $sp_part){ ?>
 
-                    <div class="row justify-content-center">
+                    <div class="row text-center">
 
                         <div class="col-12 single_post_title">
                             <?php echo "<h2>" . $sp_part["title"] . "</h2>"; ?>
@@ -36,13 +33,13 @@ include '../classes/Single_post.php';
 
                     </div>
 
-                    <div class="row justify-content-center">
+                    <div class="row text-center">
 
                         <div class="col-6 single_post_image_frame">
                             <img class="single_post_image" src="../includes/<?php echo $sp_part['image']; ?>" alt="blog post image">
                         </div>
 
-                        <div class="col-6 single_post_description">
+                        <div class="col-6 text-center single_post_description">
                             <?php echo "<p>" . $sp_part["description"] . "</p>"; ?>
                         </div>
 
@@ -50,22 +47,25 @@ include '../classes/Single_post.php';
 
                     <div class="row">
                             
-                        <td class="col-12 single_post_category_date">
-                            <?php echo "<p class='single_post_category'>" . $sp_part["category"] . "</p>" . " "; ?>
-                            
-                            <?php echo "<p class='single_post_date'>" . $sp_part["post_date"] . "</p>"; ?>
-                        </td>
+                        <div class="col-6 single_post_category">
+                            <?php echo "<p>" . "Category: " . $sp_part["category"] . "</p>" . " "; ?>
+                        </div>  
 
                     </div>
 
-        <?php
-        }
+                    <div class="row">
 
+                        <div class="col-6 single_post_date">
+                            <?php echo "<p>" . $sp_part["post_date"] . "</p>"; ?>
+                        </div>
+                    </div>
+
+        <?php
+            }
         ?>
 
-    <!--</div>
 
-</div>-->
+
 </main>
 <?php
 // Include footer
