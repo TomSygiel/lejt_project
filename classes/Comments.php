dc51aa7de86ccbc5df6091df56c9d7981a5fd237
JuliaLahdo <julia.lahdo@live.se>
2018-12-07 10:51:05 +0100

    remove some previous unused code

--- classes/Comments.php
+++ classes/Comments.php
@@ -13,21 +13,7 @@ class Comments
     {
         $this->pdo = $pdo;
     }
-    /*public function register($username, $password) 
-    { 
 
-        try{
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-            $stmt = $this->pdo->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
-
-            $stmt->execute(     
-                [
-                    ":username" => $username,
-                    ":password" => $hashed_password
-
-                ]);
-            return true;
-        }*/
     public function postComment($content, $post_id, $created_by, $comment_date)
     {
         $statement = $this->pdo->prepare("INSERT INTO comments (content, post_id, created_by, comment_date) VALUES (:content, :post_id, :created_by, :comment_date)");
