1b35575e4340bac9d3ab6f23e9c700d4ad79edc9
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2018-12-08 16:55:52 +0100

    Remove php from html pages and isert them as includes instead
    It affects error handling and fetch from data base on new post and edit post pages

--- includes/post_server.php
+++ includes/post_server.php
@@ -5,8 +5,6 @@ session_start();
 require 'database_connection.php';
 require '../classes/Posts.php';
 
-//var_dump($_FILES);
-
 $image = $_FILES["image"];
 $title = $_POST["title"];
 $description = $_POST["description"];
@@ -22,7 +20,6 @@ $upload_ok = move_uploaded_file($temporary_location, $new_location);
 
 $posts = new Post($pdo);
 $posts->blogPost($new_location, $title, $description, $created_by, $category, $post_date);
-//$posts->deletePost();
 
 header ('Location: ../index.php');
 
