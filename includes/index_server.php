0a19fa62d2aad145c9add09cb9b4b21162f6c1cb
lindastorgard <linda.storgard@gmail.com>
2018-12-09 10:05:05 +0100

    add index page for logged in as user, modifications to index page, if user is user show user page - if user is admin show admin page

---
+++ includes/index_server.php
@@ -0,0 +1,11 @@
+<?php
+
+// Include database connection
+include ("includes/database_connection.php");
+
+// Fetch selected posts from database
+$fetch_all_posts_statement = $pdo->prepare("SELECT * FROM `posts` ORDER BY post_id DESC LIMIT 3");
+$fetch_all_posts_statement->execute();
+$all_posts = $fetch_all_posts_statement->fetchAll(PDO::FETCH_ASSOC);
+
+?>
