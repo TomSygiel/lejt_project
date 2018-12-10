cec3faa9636047c6837ef3f749c0f5c2d79a5a41
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2018-12-10 16:32:07 +0100

    Error handling for register site
    Removes unnecesary code lines

--- includes/register_server.php
+++ includes/register_server.php
@@ -5,35 +5,31 @@ include ("../classes/Register.php");
 
 $register = new Register($pdo);
 
+if (isset($_POST["registration"]) && (empty($_POST['username']) || empty($_POST['password']))){
+
+    // If username or password left empty , registration fails, efter 3 second user gets redirected to register.php
+    header ('Location:../views/register.php?error=empty');
+    exit();
+}
 
 if (!empty($_POST['username']) && !empty($_POST['password'])){
 
-
-
     $find_user = $register->fetched_user($_POST["username"]);
     
-    if ($_POST['username'] == $find_user["username"]){
+        if ($_POST['username'] == $find_user["username"]){
 
-        echo "<p class='error'>* Username is already taken. </p>";
-        // username is alreday in database, efter 3 second go back to register.php for try again
-        header ('refresh:3;url=../views/register.php?error=usernametaken');
-        exit();
-    }
-    
-    if (empty($_POST['username']) || empty($_POST['password'])){
-        echo "<p class='error'>* Registration failed, please fill in username and password. </p>";
-        // if user is not write in username or password, registration failed, efter 3 second go to register.php
-        header ('refresh:3;url=../views/register.php?error=empty');
-        exit();
-    }
-    
-    else{
-        $register->register($_POST["username"], $_POST["password"]);
-        // user filled in username and password, registration succed, efter 3 second go to index.php
-        echo "<p class='error'>* Registration Completed.</p>";
-        header ('refresh:3;url=../index.php?succeed');
-        exit();
-    }    
+            // username is alreday in database, efter 3 second go back to register.php for try again
+            header ('Location:../views/register.php?error=usernametaken');
+            exit();
+        }
+        
+        else{
+            $register->register($_POST["username"], $_POST["password"]);
+            // user filled in username and password, registration succeed, efter 3 second go to index.php
+            echo "<p class='notice'>* Registration Completed.</p>";
+            header ('Location:../views/register.php?notice=succeed');
+            exit();
+        }    
 
 }
 
