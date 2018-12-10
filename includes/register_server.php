13a44a282baec1fc21be880cbd0bbbc4f7a63500
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2018-12-10 16:45:24 +0100

    Cleaning job

--- includes/register_server.php
+++ includes/register_server.php
@@ -16,22 +16,21 @@ if (!empty($_POST['username']) && !empty($_POST['password'])){
 
     $find_user = $register->fetched_user($_POST["username"]);
     
-        if ($_POST['username'] == $find_user["username"]){
+    if ($_POST['username'] == $find_user["username"]){
 
-            // username is alreday in database, efter 3 second go back to register.php for try again
-            header ('Location:../views/register.php?error=usernametaken');
-            exit();
-        }
+        // username is alreday in database, efter 3 second go back to register.php for try again
+        header ('Location:../views/register.php?error=usernametaken');
+        exit();
+    }
         
-        else{
-            $register->register($_POST["username"], $_POST["password"]);
-            // user filled in username and password, registration succeed, efter 3 second go to index.php
-            echo "<p class='notice'>* Registration Completed.</p>";
-            header ('Location:../views/register.php?notice=succeed');
-            exit();
-        }    
+    else{
+        $register->register($_POST["username"], $_POST["password"]);
+        // user filled in username and password, registration succeed, efter 3 second go to index.php
+        echo "<p class='notice'>* Registration Completed.</p>";
+        header ('Location:../views/register.php?notice=succeed');
+        exit();
+    }    
 
 }
-
 
 ?>
