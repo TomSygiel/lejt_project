c7e9d1a3947d9c076c355548594225546f14b15f
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2018-12-11 21:38:27 +0100

    Complete error handling for blog post to check uploaded file format and size

--- classes/Posts.php
+++ classes/Posts.php
@@ -59,35 +59,41 @@ public function deletePostAndComments() {
     
 //Create new post with error handling
 
-    public function blogPost($new_location, $title, $description, $created_by, $category, $post_date) {
+    public function blogPost($new_location, $title, $description, $created_by, $category, $post_date, $upload_ok) {
 
         if (empty($title) && empty($description)) {
 
-            header('Location: ../views/new_post_form.php?error=empty');
+            header('Location: ../views/new_post_form.php?error=empty&title='.$title.'&description='.$description.'&category='.$category);
             exit();
             
-        } if (empty($title)) {
+        } 
         
-            header('Location: ../views/new_post_form.php?error=notitle');
+        if (empty($title)) {
+        
+            header('Location: ../views/new_post_form.php?error=notitle&title='.$title.'&description='.$description.'&category='.$category);
             exit();
             
-        } if (empty($description)) {
+        } 
         
-            header('Location: ../views/new_post_form.php?error=nodescription');
+        if (empty($description)) {
+        
+            header('Location: ../views/new_post_form.php?error=nodescription&title='.$title.'&description='.$description.'&category='.$category);
             exit();
 
-        } if (empty($category)) {
+        } 
         
-            header('Location: ../views/new_post_form.php?error=nocategory');
+        if (empty($category)) {
+        
+            header('Location: ../views/new_post_form.php?error=nocategory&title='.$title.'&description='.$description.'&category='.$category);
             exit();
             
-        }
-
+        } 
+        
         if (isset($_POST["post"])) {
             $title = strip_tags($_POST["title"]);
             $description = strip_tags($_POST["description"]);
 
-            if (!empty($title) && !empty($description)) {
+            if ($upload_ok && !empty($title) && !empty($description)) {
                 $statement = $this->pdo->prepare("INSERT INTO posts (title, description, created_by, image, category, post_date) VALUES(:title, :description, :created_by, :image, :category, :post_date)");
                 $statement->execute(
                         [
@@ -100,7 +106,7 @@ public function deletePostAndComments() {
                         ]
                     );
 
-                    return true;
+                return true;
 
              } 
         
